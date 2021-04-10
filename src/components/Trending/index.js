7417bb86289c85735bd52f5dfc2f982b40db2a92
Bassam Rubaye <bassam.rubaye@gmail.com>
2021-04-09 18:42:49 -0700

    Add Treanding page

---
+++ src/components/Trending/index.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import {
+  Card,
+  CardMedia,
+  CardHeader,
+  GridList,
+  GridListTile,
+  GridListTileBar,
+} from "@material-ui/core";
+import ImageList from "../ImageList";
+const URL_TRENDING = `${process.env.REACT_APP_GIPHY_URL}gifs/trending?api_key=${process.env.REACT_APP_GIPHY_API_KEY}`;
+// "https://api.giphy.com/v1/gifs/trending?api_key=M2xmRhz0VZaCUg3A8iw7PTCj0RnU590i";
+
+export default function Trending() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchTrending = () => {
+      fetch(URL_TRENDING)
+        .then((res) => res.json())
+        .then((da) => {
+          setData(da.data);
+
+          console.log(da);
+        });
+    };
+
+    fetchTrending();
+  }, []);
+
+  const renderCards = (images) => {
+    return (
+      <GridList>
+        {images.map((d, i) => (
+          <GridListTile key={d.id}>
+            <img src={d.images.downsized.url} />
+            <GridListTileBar title={d.title} />
+          </GridListTile>
+        ))}
+      </GridList>
+    );
+  };
+  return (
+    <div>
+      <h2>Trending Gifs</h2>
+      <ImageList images={data} />
+    </div>
+  );
+}
