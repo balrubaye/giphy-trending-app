ff8cca0be969a6d72809e21a8a4e827c69ee95a2
Bassam Rubaye <bassam.rubaye@gmail.com>
2021-04-09 18:41:15 -0700

    Add imageList component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Layout from "./components/Layout";
+import Trending from "./components/Trending";
+import Search from "./components/Search";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout>
+        <Switch>
+          <Route path="/trending">
+            <Trending />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+        </Switch>
+      </Layout>
+    </Router>
   );
 }
 
---
+++ src/components/ImageList/index.js
@@ -0,0 +1,24 @@
+import {
+  TextField,
+  Button,
+  Typography,
+  Grid,
+  GridList,
+  GridListTile,
+  GridListTileBar,
+} from "@material-ui/core";
+import useStyles from "./styles";
+
+export default function ImageList({ images }) {
+  const classes = useStyles();
+  return (
+    <GridList>
+      {images.map((d, i) => (
+        <GridListTile className={classes.gridTile} style={{ height: "480px" }}>
+          <img src={d.images.downsized.url} />
+          <GridListTileBar title={d.title} />
+        </GridListTile>
+      ))}
+    </GridList>
+  );
+}
